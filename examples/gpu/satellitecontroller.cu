491ac664001481f3b634a6dcfb5ec491fa28c1b5
kranzowe <kranzowe@gmail.com>
2026-08-10 10:52:12 -0600

    initial start

---
+++ examples/gpu/satellitecontroller.cu
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <filesystem>
+#include <vector>
+#in
+#include <string>
+#include <thread>
+#include <chrono>
+#include <cmath>
+#include "planners/KinoPaxSTAR.cuh"
+#include <thrust/count.h>
+#include <thrust/reduce.h>
+
+
+class Sat(){
+    private:
+
+
+    void step
+
+
+    public:
+        std::vector<float> m_state;
+        
+        Sat(std::vector<float> state){m_state(state)}
+
+        void step(){
+
+
+
+
+        }
+
+}
