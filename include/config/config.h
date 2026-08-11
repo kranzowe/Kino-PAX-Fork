6321ae37a38339d95f79847e1c3b6f018f3e84b9
kranzowe <kranzowe@gmail.com>
2026-08-11 16:55:14 -0600

    new cost tunes

--- include/config/config.h
+++ include/config/config.h
@@ -263,8 +263,8 @@
 #define A_MAX DV_MAX
 
 // --- Safety weight: edgeCost = dv_r^2 + dv_i^2 + W_SAFETY * sum_substep sum_def (STEP_SIZE / dist_to_center) ---
-#define W_SAFETY 0.05f
-#define W_TIME 0.005f
+#define W_SAFETY 0.5f
+#define W_TIME 0.002f
 
 // --- Syclop region grid: W_DIM=2 spatial, V_DIM=2 velocity, no attitude ---
 #define W_R1_LENGTH 24
